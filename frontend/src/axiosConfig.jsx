b589569ad7cf5c26adb8be06302663821a922e55
Wei-Yun Hsu <hjustin0602@gmail.com>
2026-04-04 15:11:53 +1000

    update axiosConfig.jsx baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.27.173.18:5001', // live
+  baseURL: 'http://52.65.114.71:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
